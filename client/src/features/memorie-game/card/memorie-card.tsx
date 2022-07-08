6413e0c3cef4e6d5f56a65214fe71208aaba4616
Yannick Großkreutz <grosskreutzyannick@gmail.com>
2022-07-08 17:20:39 +0200

    Chore: changed MemorieCard to class

--- client/src/features/memorie-game/card/memorie-card.tsx
+++ client/src/features/memorie-game/card/memorie-card.tsx
@@ -4,14 +4,24 @@ interface MemorieCardProps {
     id: string;
 }
 
-const MemorieCard: FC<MemorieCardProps> = ({id}) => {
-  return (
-    <>
-        <div className='memorie-card' data-testid={`memorie-card-${id}`}>
-            
-        </div>
-    </>
-  );
-};
+interface MemorieCardState {
+    flipped: boolean; //if true, show image of card
+}
+
+
+class MemorieCard extends React.Component<MemorieCardProps, MemorieCardState> {
+
+
+    render(): React.ReactNode {
+        return (
+            <>
+                <div className='memorie-card' data-testid={`memorie-card-${this.props.id}`}>
+                    
+                </div>
+            </>
+          );        
+    }
+
+}
 
 export default MemorieCard;
