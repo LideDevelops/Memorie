0bdfa62ac6a413ae3b11463ff295e7eb6dde536c
Yannick Großkreutz <grosskreutzyannick@gmail.com>
2022-07-08 16:07:20 +0200

    Chore: Changed to typescript template

--- client/src/feature/memorie/memorie-card.js
+++
@@ -1,9 +0,0 @@
-
-function MemorieCard() {
-  return (
-    <div className="memorie-card" data-testid="memorie-card">
-    </div>
-  );
-}
-
-export default MemorieCard;
--- client/src/feature/memorie/memorie-card.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import MemorieCard from './memorie-card';
-
-test('Draw Memorie card', async () => {
-  render(<MemorieCard />);
-  const linkElement = await screen.findByTestId("memorie-card");
-  expect(linkElement).toBeInTheDocument();
-  expect(linkElement).toBeVisible();
-});
