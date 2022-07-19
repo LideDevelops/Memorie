511653b8088992fb187d1a9eea62674a27458e32
CuriousOtter <grosskreutzyannick+github@gmail.com>
2022-07-19 21:11:55 +0200

    Feat: started development on backend server

---
+++ server/CuriousOtter.Memorie/CuriousOtter.Memorie.Domain/Models/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuriousOtter.Memorie.Domain.Models
+{
+    public abstract class BaseEntityy<TIdentidfier> 
+    {
+        public DateTime LastModified { get; set; }
+    }
+}
