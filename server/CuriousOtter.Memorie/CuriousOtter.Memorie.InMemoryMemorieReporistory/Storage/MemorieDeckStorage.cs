2be580ea0f47c76dadf5cec0cb87e243b76a603b
CuriousOtter <grosskreutzyannick+github@gmail.com>
2022-07-22 20:05:03 +0200

    Feat: prepared in memory persitance instance

---
+++ server/CuriousOtter.Memorie/CuriousOtter.Memorie.InMemoryMemorieReporistory/Storage/MemorieDeckStorage.cs
@@ -0,0 +1,21 @@
+﻿using CuriousOtter.Memorie.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuriousOtter.Memorie.InMemoryMemorieReporistory.Storage
+{
+    public class MemorieDeckStorage
+    {
+        private ICollection<MemorieDeck> memorieDecks;
+
+        public MemorieDeckStorage()
+        {
+            this.memorieDecks = new List<MemorieDeck>();
+        }
+
+
+    }
+}
