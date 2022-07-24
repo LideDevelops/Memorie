dad991b13cd022d446d8956a0145df5ef57e9716
CuriousOtter <grosskreutzyannick+github@gmail.com>
2022-07-24 13:15:34 +0200

    Feat: created class for dependency injection for in memorie storage

--- server/CuriousOtter.Memorie/CuriousOtter.Memorie.GameAPI/Program.cs
+++ server/CuriousOtter.Memorie/CuriousOtter.Memorie.GameAPI/Program.cs
@@ -1,3 +1,5 @@
+using CuriousOtter.Memorie.InMemoryMemorieReporistory.Setup;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddInMemoryMemorieRepositroy();
 
 var app = builder.Build();
 
---
+++ server/CuriousOtter.Memorie/CuriousOtter.Memorie.InMemoryMemorieReporistory/Setup/ServiceCollectionExtension.cs
@@ -0,0 +1,53 @@
+﻿using CuriousOtter.Memorie.Domain.Models;
+using CuriousOtter.Memorie.Infrastructure.Ports.Memorie;
+using CuriousOtter.Memorie.InMemoryMemorieReporistory.Repository.MemorieDeck;
+using CuriousOtter.Memorie.InMemoryMemorieReporistory.Storage;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuriousOtter.Memorie.InMemoryMemorieReporistory.Setup
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddInMemoryMemorieRepositroy(this IServiceCollection services)
+        {
+
+
+            services.AddSingleton<IMemorieDeckStorage, MemorieDeckStorage>(factory =>
+            {
+                var initialMemorieDeckList = new List<MemorieDeck>()
+                {
+                    new MemorieDeck(1)
+                    {
+                        LastModified = DateTime.Now,
+                        CardsWithoutDoubles = new MemorieCard[]
+                        {
+                            new MemorieCard(1)
+                            {
+                                LastModified = DateTime.Now,
+                                Name = "Test Card 1"
+                            },
+                            new MemorieCard(2)
+                            {
+                                LastModified = DateTime.Now,
+                                Name = "Test Card 1"
+                            },
+                            new MemorieCard(3)
+                            {
+                                LastModified = DateTime.Now,
+                                Name = "Test Card 1"
+                            },
+                        }
+                    }
+                };
+                return new MemorieDeckStorage(initialMemorieDeckList);
+            });
+            services.AddTransient<IMemorieDeckRetrieverRepository, MemorieDeckRepository>();
+            return services;
+        }
+    }
+}
